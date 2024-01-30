4397d94d43a8de141a5c078f1f2ae1f8def20ec4
Kreoniz <kreoniz@mail.ru>
2024-01-30 10:38:39 +0600

    Add iterative solution to fibonacci problem

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+function fibs(n) {
+  const sequence = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+  
+  return sequence;
+}
+
+console.log('Iterator:', fibs(8));
